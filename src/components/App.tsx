93e8d1c9b49fb6c257c349642af44293e3b92cec
high1 <radomir.simic@gmail.com>
2020-06-24 12:22:48 +0200

    Added wordmark instead of a plain link
    - used wordmark in place of the plaintext link
    - use class instead of className for styling
    - updated packages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,18 @@
 import { Component } from 'solid-js';
 import logo from '../assets/logo.svg';
+import wordmark from '../assets/wordmark.svg';
 import './App.css';
 
 const App: Component = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div class="App">
+      <header class="App-header">
+        <img src={logo} class="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://solidjs.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
+        <a class="App-link" href="https://solidjs.com" target="_blank" rel="noopener noreferrer">
+          <img src={wordmark} class="App-wordmark" alt="wordmark" />
         </a>
       </header>
     </div>
